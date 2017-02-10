ae4fc467de5dd61c772b6ec0f87fa3e2601bd7dd
Timo Sulg <timgluz@gmail.com>
2017-02-10 15:48:08 +0100

    refactor into commands patterns; add scan command;
    Signed-off-by: Timo Sulg <timgluz@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -22,30 +22,18 @@ fn init_out_file(outfile_path: &Path) -> Result<bool, std::io::Error> {
     Ok(true)
 }
 
-fn do_scan_task(dir_txt: &str, outpath: Option<String>) -> Result<bool, std::io::Error>{
-    println!("Scanning: {}", dir_txt);
-    let dir = Path::new(dir_txt);
 
-    if outpath.is_some() {
-        println!("Will dump results into file...");
-        let path_str = outpath.unwrap();
-        let out_dir =  Path::new(&path_str);
-        let res = init_out_file(&out_dir);
-        match res {
-            Ok(_)   => checker::scan_dir(dir, Some(&out_dir)),
-            Err(e)  => Err(e)
-        };
 
-    } else {
-        println!("No output file were defined");
-        checker::scan_dir(dir, None);
-    }
-    Ok(true)
-}
-
-fn show_usage(program_name: &str, opts: Options){
-    let brief = format!("usage: {} scan DIRECTORY_PATH [options]", program_name);
+fn show_usage(program_name: &str, opts: Options) -> Result<bool, String> {
+    let brief = format!(r#"
+        usage:
+            {} scan DIRECTORY_PATH -o OUTPUT_FILE
+            {} lookop FILE_SHA -a API_TOKEN
+        "#,
+        program_name, program_name
+    );
     print!("{}", opts.usage(&brief));
+    Ok(true)
 }
 
 fn main() {
@@ -55,6 +43,7 @@ fn main() {
 
     //register options
     opts.optopt("o", "output", "specifies the name of output file", "FILENAME");
+    opts.optopt("a", "auth", "specifies api-key for API calls", "API_TOKEN");
     opts.optflag("h", "help", "shows usage help");
 
     //parse command-line arguments
@@ -69,18 +58,73 @@ fn main() {
         return;
     }
 
-    //extract input arguments
-    let output = matches.opt_str("o");
-    let input = if !matches.free.is_empty(){
-        matches.free[0].clone()
-    } else {
+    if matches.free.len() < 1 {
+        println!("Error: Subcommand is unspecified");
         show_usage(&program_name, opts);
         return;
+    }
+
+    let command = matches.free[0].clone();
+    //println!("User asked command: {}", command);
+
+    match command.as_ref() {
+        "scan"      => do_scan_task(&matches),
+        "lookup"    => do_lookup_task(&matches),
+        _           => show_usage(&program_name, opts)
     };
 
-    // fire the task
-    match do_scan_task(&input, output){
+    println!("Done")
+}
+
+
+fn do_scan_task(matches: &getopts::Matches) -> Result<bool, String> {
+    //extract input arguments
+    let dir_txt = if matches.free.len() != 2 {
+        panic!("scan command misses a path to folder".to_string());
+    } else {
+        matches.free[1].clone()
+    };
+    let outpath = matches.opt_str("o");
+
+    println!("Scanning: {}", dir_txt);
+    let dir = Path::new(&dir_txt);
+
+    if outpath.is_some() {
+        println!("Will dump results into file...");
+        let path_str = outpath.unwrap();
+        let out_dir =  Path::new(&path_str);
+        let res = init_out_file(&out_dir);
+        let cmd_res = match res {
+            Ok(_)   => checker::scan_dir(dir, Some(&out_dir)),
+            Err(e)  => Err(e)
+        };
+        print_cmd_result(cmd_res);
+
+    } else {
+        println!("No output file were defined");
+        checker::scan_dir(dir, None);
+    }
+    Ok(true)
+}
+
+fn do_lookup_task(matches: &getopts::Matches) -> Result<bool, String> {
+
+    let file_sha = if matches.free.len() != 2 {
+        panic!("lookup command misses SHA-code");
+
+    } else {
+        matches.free[1].clone()
+    };
+    let api_key = matches.opt_str("a");
+
+    println!("Going to checkup product details by SHA: #{}", file_sha);
+
+    Ok(true)
+}
+
+fn print_cmd_result(cmd_res: Result<bool, std::io::Error>){
+    match cmd_res {
         Ok(res) => println!("Done!"),
-        Err(e)  => println!("Failed to scan folders")
+        Err(e)  => println!("Failed to finish the task")
     };
 }
