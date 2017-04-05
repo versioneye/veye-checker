f97face9d4f150d19d920e07b52abd586c67931e
Timo Sulg <timgluz@gmail.com>
2017-04-05 14:25:09 +0200

    add release script for OSx

---
+++ provision/osx/make_osx_release.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+REPO_URL="https://github.com/versioneye/veye-checker.git"
+WORK_DIR="${HOME}/Ideaprojects/veye-checker/provision/osx/veye-checker"
+TEST_DIR="${WORK_DIR}/tests/acceptance"
+
+RELEASE_DIR="${HOME}/Ideaprojects/releases"
+TIMESTAMP=$(date +"%s")
+
+if [[ -n "$RELEASE_VERSION" ]]; then
+    RELEASE_VERSION="_build_${TIMESTAMP}"
+fi
+
+RELEASE_PATH="${RELEASE_DIR}/veye_checker_x86_64_${RELEASE_VERSION}"
+
+if [ ! -d "$WORK_DIR" ]; then
+    git clone ${REPO_URL}
+fi
+
+echo "Pulling latest code from master"
+cd ${WORK_DIR}
+git pull
+
+if [ ! -d "temp" ]; then
+    echo "Add temp folder to keep test results"
+    mkdir -p temp
+fi
+
+echo "Running unit-tests"
+cargo test
+if [ $? -ne 0 ]; then
+    echo "Failed to pass unit tests"
+    exit
+fi
+
+echo "Compiling debug version"
+cd ${WORK_DIR}
+cargo build
+
+#ps: files are ordered differently on OSx
+echo "Running acceptance tests against debug release..."
+cd ${TEST_DIR}
+export VERSIONEYE_BIN_PATH="../../target/debug/veye_checker"
+bash tests_osx.sh
+if [ $? -ne 0 ]; then
+    echo "Failed to pass acceptance tests on debug release"
+    exit
+fi
+
+echo "release binary into ${RELEASE_PATH}"
+cd ${WORK_DIR}
+cargo build --release
+cp target/release/veye_checker ${RELEASE_PATH}
---
+++ tests/acceptance/tests_osx.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+. assert.sh
+
+# run as VERSIONEYE_API_KEY="yourkey" ./run.sh
+[ -z "$VERSIONEYE_API_KEY" ] && echo "Need to set VERSIONEYE_API_KEY" && exit 1;
+
+if [ -z ${VERSIONEYE_BIN_PATH+x} ]; then
+    VERSIONEYE_BIN_PATH="../../target/debug/veye_checker"
+    echo "Using default binary ${VERSIONEYE_BIN_PATH}"
+else
+    echo "Using specified binary at ${VERSIONEYE_BIN_PATH}"
+fi
+
+FIXTURES_PATH="../fixtures/files"
+FILE_SHA="5675fd96b29656504b86029551973d60fb41339b"
+
+echo "#-- Going to execute acceptance tests"
+echo "#-- shas command"
+assert "exec ${VERSIONEYE_BIN_PATH} shas ${FIXTURES_PATH}" "filepath,packaging,sha_method,sha_value\n../fixtures/files/test.nupkg,nupkg,sha512,U82mHQSKaIk+lpSVCbWYKNavmNH1i5xrExDEquU1i6I5pV6UMOqRnJRSlKO3cMPfcpp0RgDY+8jUXHdQ4IfXvw==\n../fixtures/files/test.jar,jar,sha1,5675fd96b29656504b86029551973d60fb41339b\nDone!"
+
+echo "#-- lookup command"
+assert "exec ${VERSIONEYE_BIN_PATH} lookup ${FILE_SHA}" \
+ "filepath,packaging,sha_method,sha_value,language,prod_key,version,n_vulns,product_url,license,error\n,,,5675fd96b29656504b86029551973d60fb41339b,java,commons-beanutils/commons-beanutils,1.7.0,1,https://www.versioneye.com/Java/commons-beanutils:commons-beanutils/1.7.0,unknown,\nDone!"
+
+echo "#-- resolve command"
+assert "exec ${VERSIONEYE_BIN_PATH} resolve ${FIXTURES_PATH}" \
+    "filepath,packaging,sha_method,sha_value,language,prod_key,version,n_vulns,product_url,license,error\n../fixtures/files/test.nupkg,nupkg,sha512,U82mHQSKaIk+lpSVCbWYKNavmNH1i5xrExDEquU1i6I5pV6UMOqRnJRSlKO3cMPfcpp0RgDY+8jUXHdQ4IfXvw==,csharp,Newtonsoft.Json,9.0.1,0,https://www.versioneye.com/CSharp/Newtonsoft~Json/9.0.1,MIT,\n../fixtures/files/test.jar,jar,sha1,5675fd96b29656504b86029551973d60fb41339b,java,commons-beanutils/commons-beanutils,1.7.0,1,https://www.versioneye.com/Java/commons-beanutils:commons-beanutils/1.7.0,unknown,\nDone!"
+
+echo "#-- usage message"
+assert "exec ${VERSIONEYE_BIN_PATH} -h" \
+    "\n        usage:\n            ../../target/debug/veye_checker resolve DIRECTORY_TO_SCAN -o OUTPUT_FILE -a API_TOKEN\n            ../../target/debug/veye_checker shas DIRECTORY_PATH -o OUTPUT_FILE\n            ../../target/debug/veye_checker lookup FILE_SHA -a API_TOKEN\n        \n\nOptions:\n    -o, --output FILENAME\n                        specifies the name of output file\n    -a, --auth API_TOKEN\n                        specifies the api-key for API calls\n    -h, --help          shows usage help"
+assert_end
