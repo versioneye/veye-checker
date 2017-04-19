668358ef72fa46248cf5275e577b14951d5a9879
Timo Sulg <timgluz@gmail.com>
2017-04-19 11:31:30 +0200

    fix acceptance tests for linux; update linux vagrant set up script;

--- provision/x64/setup_ubuntu_box.sh
+++ provision/x64/setup_ubuntu_box.sh
@@ -2,8 +2,11 @@
 
 #update package registry
 sudo apt-get update
-sudo apt-get install -y curl g++ git
+sudo apt-get install -y curl g++ git openssl libssl-dev pkg-config
 
 #install rust
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
+rustup toolchain install nightly
+rustup default nightly
